731e108f2d37bb0c363089f0adf0c9bfffc8744f
Asanga Manpitiya <asanga.manpitiya@capgemini.com>
2021-02-18 09:23:11 +0100

    add input reading & error handling

--- APIRateLimitChecker/Program.cs
+++ APIRateLimitChecker/Program.cs
@@ -8,13 +8,15 @@ namespace APIRateLimitChecker
 {
     public class Program
     {
-        static string token = "ccb855827aec1b7c5cf3460d1ca43ec5c29310af";
-        static string baseUrl = "https://api.github.com";
-        static decimal percentage = 10;
+        static string token = String.Empty;
+        static readonly string baseUrl = "https://api.github.com";
+        static readonly decimal percentage = 10;
 
         static async Task Main(string[] args)
         {
-            Console.WriteLine("This Apllication checks Github API's available rate. If it available rate is less than 10%, it will print 1. else 0 ");
+            Console.WriteLine(string.Format("This Apllication checks Github API's rate limit. If the available rate is less than {0}%, it will print 1. else 0 ", percentage));
+            Console.Write("Please Enter the Github Personal Access Token (PAT): ");
+            token = Console.ReadLine();
             await APIRateLimitCheckAsync();
             Console.ReadLine();
         }
@@ -30,8 +32,16 @@ namespace APIRateLimitChecker
 
             var rate = await ExecuteAPICallAsync(client);
 
-            var exceeded = IsRateLimitExceed(rate, percentage);
-            Console.WriteLine(Convert.ToByte(exceeded));
+            if (rate != null)
+            {
+                var exceeded = IsRateLimitExceed(rate, percentage);
+                Console.WriteLine(Convert.ToByte(exceeded));
+            }
+            else
+            {
+                //Error occured. Cannot calculate teh available rate.
+                Console.WriteLine("-1");
+            }
         }
 
         public static bool IsRateLimitExceed(dynamic rate, decimal percentage)
@@ -44,12 +54,31 @@ namespace APIRateLimitChecker
 
         public static async Task<dynamic> ExecuteAPICallAsync(HttpClient client)
         {
+            dynamic rate = null; 
+            try
+            {
+                var response = await client.GetAsync("/rate_limit");
 
-            var response = await client.GetAsync("/rate_limit");
-            var contents = await response.Content.ReadAsStringAsync();
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var contents = await response.Content.ReadAsStringAsync();
 
-            var result = JsonConvert.DeserializeObject<dynamic>(contents);
-            var rate = result.rate;
+                    var result = JsonConvert.DeserializeObject<dynamic>(contents);
+                    rate = result.rate;
+                }
+                else if(response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                {
+                    Console.WriteLine(response.StatusCode.ToString() + ". Please check your PAT is correct...");
+                }
+                else
+                {
+                    Console.WriteLine(response.StatusCode.ToString());
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             return rate;
         }
