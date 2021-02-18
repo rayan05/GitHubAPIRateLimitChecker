54ce0b21e9f5a0e3ea5ef4df703fa7ee8406d6fa
Asanga Manpitiya <asanga.manpitiya@capgemini.com>
2021-02-18 08:33:38 +0100

    Add coding & unit tests

--- APIRateLimitChecker/Program.cs
+++ APIRateLimitChecker/Program.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace APIRateLimitChecker
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static string token = "ccb855827aec1b7c5cf3460d1ca43ec5c29310af";
+        static string baseUrl = "https://api.github.com";
+        static decimal percentage = 10;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This Apllication checks Github API's available rate. If it available rate is less than 10%, it will print 1. else 0 ");
+            await APIRateLimitCheckAsync();
+            Console.ReadLine();
+        }
+
+        public static async Task APIRateLimitCheckAsync()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(baseUrl);
+
+            client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("AppName", "1.0"));
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", token);
+
+            var rate = await ExecuteAPICallAsync(client);
+
+            var exceeded = IsRateLimitExceed(rate, percentage);
+            Console.WriteLine(Convert.ToByte(exceeded));
+        }
+
+        public static bool IsRateLimitExceed(dynamic rate, decimal percentage)
+        {
+            decimal limit = rate.limit;
+            decimal remaining = rate.remaining;
+
+            return ((remaining / limit) * 100) < percentage;
+        }
+
+        public static async Task<dynamic> ExecuteAPICallAsync(HttpClient client)
+        {
+
+            var response = await client.GetAsync("/rate_limit");
+            var contents = await response.Content.ReadAsStringAsync();
+
+            var result = JsonConvert.DeserializeObject<dynamic>(contents);
+            var rate = result.rate;
+
+            return rate;
         }
     }
 }
--- APIRateLimitCheckerTest/UnitTest1.cs
+++ APIRateLimitCheckerTest/UnitTest1.cs
@@ -1,18 +1,111 @@
 using NUnit.Framework;
+using APIRateLimitChecker;
+using System.Net.Http;
+using Moq;
+using System.Threading.Tasks;
+using Moq.Protected;
+using System.Threading;
+using System.Net;
+using System;
+using System.Dynamic;
 
 namespace APIRateLimitCheckerTest
 {
     public class Tests
     {
+        StringContent content;
         [SetUp]
         public void Setup()
         {
+            content = new StringContent(@"{
+  'resources': {
+    'core': {
+      'limit': 5000,
+      'remaining': 4999,
+      'reset': 1372700873
+    },
+                   'search': {
+                'limit': 30,
+      'remaining': 18,
+      'reset': 1372697452
+                   },
+    'graphql': {
+                'limit': 5000,
+      'remaining': 4993,
+      'reset': 1372700389
+    },
+    'integration_manifest': {
+                'limit': 5000,
+      'remaining': 4999,
+      'reset': 1551806725
+    },
+    'code_scanning_upload': {
+                'limit': 500,
+      'remaining': 499,
+      'reset': 1551806725
+    }
+        },
+  'rate': {
+    'limit': 5000,
+    'remaining': 4999,
+    'reset': 1372700873
+  }
+}");
         }
 
         [Test]
-        public void Test1()
+        public async Task TestAPICall()
         {
-            Assert.Pass();
+            // Arrange
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+               .Protected()
+               // Setup the PROTECTED method to mock
+               .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.IsAny<HttpRequestMessage>(),
+                  ItExpr.IsAny<CancellationToken>()
+               )
+
+               // prepare the expected response of the mocked http call
+               .ReturnsAsync(new HttpResponseMessage()
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = content,
+               })
+               .Verifiable();
+
+            var httpClient = new HttpClient(handlerMock.Object)
+            {
+                BaseAddress = new Uri("https://api.github.com"),
+            };
+
+            var result = await Program.ExecuteAPICallAsync(httpClient);
+
+            int limit = result.limit;
+            int remaining = result.remaining;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(5000, limit);
+            Assert.AreEqual(4999, remaining);
+        }
+
+        [Test]
+        public void TestRateLimt()
+        {
+            //Arrange
+            dynamic rate = new ExpandoObject();
+            rate.limit = 5000;
+            rate.remaining = 4800;
+            var percentage = 10;
+
+            //Assert
+            var result = Program.IsRateLimitExceed(rate, percentage);
+            Assert.IsFalse(result);
+
+            rate.remaining = 20;
+            var result2 = Program.IsRateLimitExceed(rate, percentage);
+            Assert.IsTrue(result2);
         }
     }
 }
